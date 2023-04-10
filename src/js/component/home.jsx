9f6f346212e1929983245f54a6f78a37679d78cd
Blancreyes <luijbr83@gmail.com>
2023-04-10 20:38:36 +0000

    Third commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,8 +8,20 @@ const API_KEY= 'rZnLT6QGSPRElrHZXpuzi5WB913YgZtr'
 const Home = () => {
 
 	const [gifs, setGifs]= useState([])
-	
+	const [keyword, setKeyword]=useState("")
+	let findgif=""
 
+
+	const handleSubmit=(e)=>{
+		e.preventDefault()
+		findgif=getGifs({keyword})
+	}
+
+
+	const handleChange=(e)=>{
+		setKeyword(e.target.value)
+	}
+	
 	const getGifs=({keyword='rick'}={})=> {
 		const REQUEST_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`
 	
@@ -30,19 +42,32 @@ const Home = () => {
 		})
 	};
 
-	useEffect( ()=>{getGifs({keyword:'john-wick'})},[]);
+	useEffect( ()=>{findgif},[handleSubmit]);
 			
 			
-	return (<div className="container">
-				<div className="d-flex flex-row overflow-scroll p-3">
-					{
-						gifs.map(({id, title, url})=><Gif 
-							key={id}
-							title={title} 
-							url={url}/>)
-					}
-				</div>
+	return (
+		<div className="container">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="search">SearchGif</label>
+				<input 
+					type="text"
+					value={keyword} 
+					onChange={handleChange}
+					
+				/>
+				<button>SEARCH</button>
+
+			</form>
+				
+			<div className="d-flex flex-row overflow-scroll p-3">
+				{
+					gifs.map(({id, title, url})=><Gif 
+						key={id}
+						title={title} 
+						url={url}/>)
+				}
 			</div>
+		</div>
 		
 	);
 };
